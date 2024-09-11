a52666febcdb44ad9132fcb294ec7cc858e3573a
Shivam Varyani <52348525+ShivVary@users.noreply.github.com>
2024-09-11 08:59:24 +0000

    uploaded exercises

---
+++ Exercises/Beginner/nodeInput.py
@@ -0,0 +1,13 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class UserInputPublisher(Node):
+	# mind goes brrr
+
+def main(args=None):
+    # Do something
+
+if __name__ == '__main__':
+    main()
+
