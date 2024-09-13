dd097e0f81b29c5ddfb4d07fd05965e02e7449f2
Thomas Nguyen <85216109+TomNgn3108@users.noreply.github.com>
2024-09-13 14:07:04 +1000

    state machine

---
+++ Exercises/Basic - State Machine/state_machine/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'state_machine'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tung',
+    maintainer_email='85216109+TomNgn3108@users.noreply.github.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'client = state_machine.state_client:main',
+            'server = state_machine.state_server:main',
+            'sub_node = state_machine.node:main'
+        ],
+    },
+)
---
+++ Exercises/Basic - State Machine/state_machine/state_machine/state_client.py
@@ -0,0 +1,33 @@
+import sys
+import rclpy
+from rclpy.node import Node
+
+from sm_srv_msg.srv import StateData  
+from sm_srv_msg.msg import StateTrans 
+from state_define import *
+class State_client(Node):
+
+    def __init__(self):
+        super().__init__('sm_client')
+        pass
+
+    def send_request(self, new_state, tran_sig):
+        pass
+
+
+def main():
+    rclpy.init()
+
+    state_machine_client = State_client()
+    new_state = state_machine_client.send_request(int(sys.argv[1]), int(sys.argv[2]))
+
+    rclpy.spin_until_future_complete(state_machine_client, new_state)
+
+    response = new_state.result()
+ 
+    state_machine_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
