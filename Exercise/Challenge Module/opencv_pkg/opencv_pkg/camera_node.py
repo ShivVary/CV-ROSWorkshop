7c3dde6a17cdaf9237de78ba59ac16f4d3dcbb04
Thomas Nguyen <85216109+TomNgn3108@users.noreply.github.com>
2025-04-03 13:54:32 +1100

    added lidar subscriber for students

--- Exercise/Challenge Module/opencv_pkg/opencv_pkg/camera_node.py
+++ Exercise/Challenge Module/opencv_pkg/opencv_pkg/camera_node.py
@@ -1,24 +1,37 @@
 import rclpy
 from rclpy.node import Node
 import cv2
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, LaserScan
 from cv_bridge import CvBridge
 
 class CCamera(Node):
-    def __init__(self):
+    def __init__(self,rate=50):
         super().__init__('camera_node')
         self.get_logger().info('CameraNode started')
         self.bridge = CvBridge()
 
-        # Initialise camera
+        # Sub to camera
         self.sub_image = self.create_subscription(Image,'/camera/image_raw',
-            self.vision_callback,10)
+            self.vision_callback,rate)
+        
+        # Sub to LiDAR
+        self.sub_lidar = self.create_subscription(LaserScan,
+            '/scan',
+            self.lidar_callback,
+            rate)
+
+    def lidar_callback(self, lidar_msg):
+        pass
+
 
     def vision_callback(self, ros_image):
         '''
         Camera topic callback 
         '''
+        # Decode and display feed
         raw_image = self.bridge.imgmsg_to_cv2(ros_image, desired_encoding='bgr8')
+        
+        # Display row feed
         cv2.imshow("Feed", raw_image)
         cv2.waitKey(3)
 
